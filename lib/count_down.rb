853a5ea689c239b1dcc48b31638ba6de3a983e92
Lauren Frankovich <frankovister@gmail.com>
2020-05-21 02:06:47 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -4,13 +4,6 @@ count = 10
 while count > 0 do
   puts #{count}
   count -= 1
+else puts "Happy New Year!"
 end 
-  puts "Happy New Year!"
   
-magic_exit_number = 7
-count = 0
-while count < 10 do
-  break if count == magic_exit_number
-  puts "I am the #{count}, I love to count!" # Work
-  count = count + 1
-end
