80648acafde1cae60ed47b357fceeb18d22aaef5
Lauren Frankovich <frankovister@gmail.com>
2020-05-21 02:05:58 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,1 +1,16 @@
 # Write your code here
+
+count = 10 
+while count > 0 do
+  puts #{count}
+  count -= 1
+end 
+  puts "Happy New Year!"
+  
+magic_exit_number = 7
+count = 0
+while count < 10 do
+  break if count == magic_exit_number
+  puts "I am the #{count}, I love to count!" # Work
+  count = count + 1
+end
